cb6b7748339cc3fca0fc3bfaa544f2d6f990f873
hrodriguez007 <heidyrodriguez567@gmail.com>
2023-10-19 15:09:39 -0500

    Handling the usecase when API request is made with the UNIX time instead

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ app.get("/api", function(req, res) {
 
 app.get("/api/:date", function(req, res) {
   let dateString = req.params.date;
+
+  if(parseInt(dateString) > 10000) {
+    let unixTime = new Date(parseInt(dateString));
+    res.json({
+      "unix": unixTime.getTime(),
+      "utc": unixTime.toUTCString()
+    });
+  }
+
+
   let passedInValue = new Date(dateString);
 
   if (passedInValue == "Invalid Date") {
